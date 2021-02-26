2ef3012b769ca19acbd24b6eaa5781ebba231695
MaykerHM <hydeki8@gmail.com>
2021-02-25 22:38:17 -0300

    day 4

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
 import styles from '../styles/components/Profile.module.css'
+import { ChallengesContext } from '../contexts/ChallengesContext'
+import { useContext } from 'react'
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext)
+
   return (
     <div className={ styles.profileContainer }>
       <img src="https://github.com/MaykerHM.png" alt="Mayker Miyanaga" />
@@ -8,7 +12,7 @@ export function Profile() {
         <strong>Mayker Miyanaga</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
-          Level 1
+          Level { level }
         </p>
       </div>
     </div>
